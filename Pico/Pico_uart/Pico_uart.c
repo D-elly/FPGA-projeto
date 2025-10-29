0bdabc2aeb7ff40be885851f5b1c90d936a92677
Johnnathan400 <johnnathan.a@acad.ifma.edu.br>
2025-10-29 10:40:29 -0300

    Update Pico_uart.c

--- Pico/Pico_uart/Pico_uart.c
+++ Pico/Pico_uart/Pico_uart.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
+uart_rx #(
+    .CLK_FREQ(50000000),
+    .BAUD_RATE(115200)
+) uart_rx_inst (
+    .clk(clk),
+    .rst(rst),
+    .rx(uart_rx_pin),
+    .data_out(received_byte),
+    .data_ready(received_flag)
+);
 
 int main()
 {
