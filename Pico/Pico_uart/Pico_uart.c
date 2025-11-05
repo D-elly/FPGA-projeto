87f91e6a94e0dce83c25ff1024355cde49b6f6ef
D-elly <danielly.s@acad.ifma.edu.br>
2025-11-04 23:40:08 -0300

    Refatoração da configuração do buzzer e implementação da tabela senoidal para geração de áudio via PWM

--- Pico/Pico_uart/Pico_uart.c
+++ Pico/Pico_uart/Pico_uart.c
@@ -14,11 +14,7 @@
 #define HEADER_BYTE 0xAA  // Byte de sincronização
 #define SAMPLE_RATE 4000           // Taxa de amostragem desejada (Hz)
 
-#define BUTTON_A 5
-#define BUTTON_B 6
-
 //Saídas do processamento
-#define BUZZER_PIN 21  // Pino conectado ao buzzer
 #define DAC_OSC_PIN 20 // Pino conecctado ao osciloscópio
 
 #define N_NOTES (sizeof(melody_notes) / sizeof(melody_notes[0]))
@@ -26,23 +22,36 @@
 #define ADC_CENTER 128               // Centro da escala de 12 bits (2^11)
 #define ADC_AMPLITUDE 127           // Amplitude maxima (2^11 - 1)
 
-const uint8_t melody_notes[] = {  // Frequencias das notas em Hz
-    255, 255, 255, 255, 255, 255, 255, 
-    255, 255, 255, 255, 255,           
-    255, 255, 255, 255, 255, 255, 255, 
-    255, 255, 255, 255, 255, 255, 255, 
-    255, 255, 255, 255, 255            
+#define BUZZER_PIN 16   // Pino conectado ao buzzer
+#define SINE_FREQ_HZ        250.0f    // Frequência alvo da senóide
+#define SAMPLES_PER_CYCLE   100       // Amostras por ciclo (→ Fs = 25 kHz)
+#define FS_HZ               (SINE_FREQ_HZ * SAMPLES_PER_CYCLE)  // 25 kHz
+
+
+// ---------- PWM ----------
+/*
+ * f_pwm = clk_sys / (clkdiv * (wrap + 1))
+ * wrap=255 (8 bits), clkdiv=1.0 → f_pwm ≈ 125e6 / 256 ≈ 488 kHz
+ */
+#define PWM_WRAP            255
+#define PWM_CLKDIV          1.0f
+
+//Tabela senoidal da nota dó(255 Hz)
+static const uint8_t sine_table[SAMPLES_PER_CYCLE] = {
+    128,136,144,153,161,169,176,183,190,196,
+    202,208,213,218,222,226,229,232,234,236,
+    237,238,238,238,237,236,234,232,229,226,
+    222,218,213,208,202,196,190,183,176,169,
+    161,153,144,136,128,119,111,102,94, 86,
+    79, 72, 65, 59, 53, 47, 42, 37, 33, 29,
+    26, 23, 21, 19, 18, 17, 17, 17, 18, 19,
+    21, 23, 26, 29, 33, 37, 42, 47, 53, 59,
+    65, 72, 79, 86, 94,102,111,119,128,136,
+    144,153,161,169,176,183,190,196
 };
 
-const uint melody_durations[] = { // Duracoes de cada nota em ms
-    500, 500, 500, 500, 500, 500, 1000, 
-    500, 500, 500, 500, 1000,           
-    500, 500, 500, 500, 500, 500, 1000,
-    500, 500, 500, 500, 500, 500, 1000,
-    500, 500, 500, 500, 1000
-};
+static volatile uint32_t s_index = 0;
 
-uint8_t queue[N_NOTES];
 volatile int counter = 0;
 volatile bool synced = false;
 volatile bool header_echo_received = false;  // NOVA FLAG
@@ -57,27 +66,16 @@ void on_uart_tx(uint8_t sample);
 // RX ativo, recebe dados do FPGA;
 void on_uart_rx();
 
-//configura tons das notas para serem reproduzidos no buzzer via PWM
-void play_tone(uint pin, uint8_t frequency, uint duration_ms);
-
-//função de chamada, coordena qual nota e duração vai ser tocada
-void play_melody(uint pin, uint8_t melody);
-
 //configura pwm para entrada do osciloscópio ser analógica
 void pwm_init_dac_osc(uint pin);
 
 //configura buzzer pin como saída pwm
 void pwm_init_buzzer(uint pin);
 
-//calcula amplitude da onda sonora da nota enviada, gerando amostra para FPGA  
-uint8_t calculate_sine_sample(uint freq, uint time_step);
-
-//função de chamada para ativar Uart TX
-bool timer_callback(struct repeating_timer *t);
+static bool sample_timer_cb(repeating_timer_t *rt);
 
 int main() {
     stdio_usb_init();
-    sleep_ms(8000);
 
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
@@ -93,8 +91,6 @@ int main() {
     for (int clear = 0; clear < 5; clear++) {
         while (uart_is_readable(UART_ID)) uart_getc(UART_ID);
     }
-
-    printf("limpou buffer\n");
     
     counter = 0;
     synced = false;
@@ -104,12 +100,14 @@ int main() {
     irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
     irq_set_enabled(UART_IRQ, true);
     uart_set_irq_enables(UART_ID, true, false);
-    printf("configurou interrupção de on_uart_rx\n");
     
-    // 2. CONFIGURACAO DO TIMER (Para amostragem precisa) em intervalos fixos (44.1kHz)
-    add_repeating_timer_us(SAMPLE_INTERVAL_US, timer_callback, NULL, &timer);
-    printf("configurou interrupção de on_uart_tx\n");
+    repeating_timer_t timer;
+    double ts_us = 1e6 / FS_HZ;               // 40 us para 25 kHz
+    int64_t interval_us = -(int64_t)(ts_us);  // negativo = periódico no SDK
 
+    if (!add_repeating_timer_us(interval_us, sample_timer_cb, NULL, &timer)) {
+        while (true) tight_loop_contents(); // fallback se falhar
+    }
     while (1) tight_loop_contents();
 }
     
@@ -117,11 +115,13 @@ void on_uart_tx(uint8_t sample) {
     while (!uart_is_writable(UART_ID)) tight_loop_contents();
 
     uart_putc_raw(UART_ID, HEADER_BYTE);
-    uart_putc_raw(UART_ID, sample);
-    printf("está mandando o sample: %i \n", sample);
+    uart_putc_raw(UART_ID, sample); 
 }
 
 void on_uart_rx() {
+    pwm_set_gpio_level(DAC_OSC_PIN, 0);
+    pwm_set_gpio_level(BUZZER_PIN, 0);
+
     while (uart_is_readable(UART_ID)) {
         int rv = uart_getc(UART_ID);
         if (rv < 0) break;
@@ -145,9 +145,7 @@ void on_uart_rx() {
             } else {
                 // primeiro byte não-header após a sincronização é o primeiro dado
                 header_echo_received = true;
-                queue[0] = byte;
-                printf("|retorno do fpga: %i| \n", queue[0]);
-                printf("Reproduzindo áudio vindo do FPGA\n");
+                printf("|retorno do fpga: %i| \n", byte);
                 pwm_set_gpio_level(DAC_OSC_PIN, byte);
                 pwm_set_gpio_level(BUZZER_PIN, byte);
                 synced = false;
@@ -157,30 +155,17 @@ void on_uart_rx() {
     }
 }
 
-void play_tone(uint pin, uint8_t frequency, uint duration_ms) {
-    uint slice_num = pwm_gpio_to_slice_num(pin);
-    uint32_t clock_freq = clock_get_hz(clk_sys);
-    uint32_t top = clock_freq / (frequency * 2.5) - 1;  //multiplicando por 2.5 para saída ser audivel
-
-    pwm_set_wrap(slice_num, 255);
-    pwm_set_gpio_level(pin, top / 2); 
-
-    pwm_set_gpio_level(pin, 0); 
-}
-
-void play_melody(uint pin, uint8_t melody) {
-        printf("Qual a nota está tocando: [%i] [%i]\n", 0, melody);
-        play_tone(pin, melody, melody_durations[0]);
-}
-
 void pwm_init_dac_osc(uint pin) {
     gpio_set_function(pin, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(pin);
     pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, 125.0f); 
+
+    pwm_set_wrap(slice_num, PWM_WRAP);
+    pwm_config_set_clkdiv(&config, PWM_CLKDIV);
     pwm_init(slice_num, &config, true);
-    pwm_set_wrap(slice_num, 255);
+
     pwm_set_gpio_level(pin, 0); 
+    pwm_set_enabled(slice_num, true);
 }
 
 void pwm_init_buzzer(uint pin) {
@@ -192,34 +177,13 @@ void pwm_init_buzzer(uint pin) {
     pwm_set_gpio_level(pin, 0); 
 }
 
-uint8_t calculate_sine_sample(uint freq, uint time_step) {
-    // Calcula a fase: 2 * PI * f * t
-    double phase = 2.0 * M_PI * (double)freq * (double)time_step * ((double)SAMPLE_INTERVAL_US / 1000000.0);
-    printf("phase: %.2f\n", phase);
-    
-    // Calcula o valor senoidal, escala para amplitude de 12 bits e centraliza em 2048
-    double value = ADC_CENTER + (double)ADC_AMPLITUDE * sin(phase);
-    printf("value: %.2f\n", value);
-    
-    // Garante que o valor nao ultrapasse o limite de 12 bits (0 a 4095)
-    if (value > 255.0) value = 255;
-    if (value < 0.0) value = 0;
+static bool sample_timer_cb(repeating_timer_t *rt) {
+    uint8_t level = sine_table[s_index];
+    s_index++;
+    if (s_index >= SAMPLES_PER_CYCLE) s_index = 0;
+    on_uart_tx(level);
 
-    return (uint8_t)round(value);
-}
-
-bool timer_callback(struct repeating_timer *t) {
-    // 1. CALCULA O SAMPLE DA ONDA SENOIDAL DA FREQUÊNCIA ATUAL
-    uint8_t sample_value = calculate_sine_sample(melody_notes[0], sample_counter);
-    printf("sample_value: %i\n", sample_value);
-
-    // 2. ENVIAR VIA UART
-    on_uart_tx(sample_value);
-    printf("Fez a chamada de on_uart_tx no callback\n");
-
-    sample_counter++;
-
-    return true; // Para que o timer continue repetindo o callback
+    return true; // continue recorrente
 }
 
 
